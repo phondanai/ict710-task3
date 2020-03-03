aa624b21f2a6df095cae5ff858bc4f17e6c3289a
Phondanai Khanti <phondanai@gmail.com>
2020-03-03 22:40:17 +0700

    Add /update /query path support

--- app/routes.py
+++ app/routes.py
@@ -5,43 +5,87 @@ from app import app, db
 from app.models import Humidity, Temperature
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    temperatures = Temperature.query.all()
-    return jsonify(json_list=[i.serialize for i in temperatures])
+    response = {
+        "sensors": {
+            "humidity": url_for("query", sensor_type="humidity", _external=True),
+            "temperature": url_for("query", sensor_type="temperature", _external=True),
+        }
+    }
+    return jsonify(response)
 
-@app.route('/temperature', methods=['GET', 'POST'])
+
+@app.route("/temperature", methods=["GET", "POST"])
 def temperature():
-    if request.method == 'POST':
-        if request.headers.get('X-API-KEY') != app.config['API_KEY']:
+    if request.method == "POST":
+        if request.headers.get("X-API-KEY") != app.config["API_KEY"]:
             abort(401)
-        if not request.json or not 'data' in request.json:
+        if not request.json or not "data" in request.json:
             abort(400)
         else:
-            if not request.json.get('data').get('temperature'):
+            if not request.json.get("data").get("temperature"):
                 abort(404)
-            t = Temperature(temperature=request.json.get('data')['temperature'])
+            t = Temperature(temperature=request.json.get("data")["temperature"])
             db.session.add(t)
             db.session.commit()
-            return jsonify({'status': 'ok'})
+            return jsonify({"status": "ok"})
     temperatures = Temperature.query.all()
     return jsonify(temperature_data=[i.serialize for i in temperatures])
 
-@app.route('/humidity', methods=['GET', 'POST'])
+
+@app.route("/humidity", methods=["GET", "POST"])
 def humidity():
-    if request.method == 'POST':
-        if request.headers.get('X-API-KEY') != app.config['API_KEY']:
+    if request.method == "POST":
+        if request.headers.get("X-API-KEY") != app.config["API_KEY"]:
             abort(401)
-        if not request.json or not 'data' in request.json:
+        if not request.json or not "data" in request.json:
             abort(400)
         else:
-            if not request.json.get('data').get('humidity'):
+            if not request.json.get("data").get("humidity"):
                 abort(404)
-            h = Humidity(humidity=request.json.get('data')['humidity'])
+            h = Humidity(humidity=request.json.get("data")["humidity"])
             db.session.add(h)
             db.session.commit()
-            return jsonify({'status': 'ok'})
+            return jsonify({"status": "ok"})
     humidity = Humidity.query.all()
 
     return jsonify(humidity_data=[i.serialize for i in humidity])
 
+
+@app.route("/update/<sensor_type>", methods=["POST"])
+def update(sensor_type):
+    if sensor_type not in ("humidity", "temperature"):
+        abort(404)
+
+    if request.headers.get("X-API-KEY") != app.config["API_KEY"]:
+        abort(401)
+
+    if not request.json or not "data" in request.json:
+        abort(400)
+
+    if sensor_type == "humidity":
+        if not request.json.get("data").get("humidity"):
+            abort(404)
+        h = Humidity(humidity=request.json.get("data")["humidity"])
+        db.session.add(h)
+        db.session.commit()
+    else:
+        if not request.json.get("data").get("temperature"):
+            abort(404)
+        t = Temperature(temperature=request.json.get("data")["temperature"])
+        db.session.add(t)
+        db.session.commit()
+
+    return jsonify({"status": "ok"})
+
+
+@app.route("/query/<sensor_type>", methods=["GET"])
+def query(sensor_type):
+
+    if sensor_type == "humidity":
+        humidity = Humidity.query.all()
+        return jsonify(humidity_data=[i.serialize for i in humidity])
+    else:
+        temperatures = Temperature.query.all()
+        return jsonify(temperature_data=[i.serialize for i in temperatures])
